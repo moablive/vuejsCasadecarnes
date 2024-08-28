5b4f65f0044fc55824c69d34a1b6b9450cb3b5ff
guilherme bonato <guilhermeferrazbonato@gmail.com>
2024-08-28 10:57:21 -0300

    update router login

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -34,7 +34,7 @@ export default defineComponent({
   methods: {
     async handleSubmit() {
       try {
-        const response = await axios.post(`${import.meta.env.VITE_API_URL}api/usuarios/login`, {
+        const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/usuarios/login`, {
           username: this.username,
           password: this.password,
         });
